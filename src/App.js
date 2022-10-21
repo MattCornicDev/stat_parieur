ba0b2ab329027d56f2057527f31f3ebaae68421b
MattCornicDev <glgharib@gmail.com>
2022-10-21 02:07:49 +0200

    desinstallation de react-boostrap pour bootstrap

--- src/App.js
+++ src/App.js
@@ -1,19 +1,15 @@
-import React from "react";
+import React, { Component } from "react";
+import  'bootstrap/dist/css/bootstrap.min.css'
+import Header from "./components/Header";
 
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-       
-        <p>
-          Ici debutera notre code 
-        </p>
-    
-         
-      </header>
-    </div>
+   <div className="App">
+    <Header />
+    <h1>Salut tout le monde</h1>
+   </div>
   );
 }
 
-export default App;
+export default App
---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+export default class Header extends Component {
+  render() {
+    return (
+      <nav className='navbar navbar-expand-sm nabar-dark bg-danger mb-3 py-0'>
+        <div className='container'>
+            <a href='/' className='navbar-brand'>Liste de contact</a>
+        </div>
+        <ul className='navbar-nav mr-auto'>
+            <li className='nav-item'>
+                <a href='/' className='nav-link'>Accueil</a>
+            </li>
+        </ul>
+      </nav>
+    )
+  }
+}
