a742d6d82a58185daf29b2b6235effb0720d8548
elodie-sz <elodie.siaze@gmail.com>
2022-11-24 11:35:08 +0100

    ajout de la navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { Actualites, Calendrier, Classement, Competitions, Equipes, Footer, Header, Joueurs, Statistiques } from './components'
-import './App.css'
+import './app.css'
 
 
 function App() {
